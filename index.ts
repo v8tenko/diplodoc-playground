15b16ec85f4f431835d4ff81d43a70a388e4dc8a
v8tenko <v8tenko@yandex-team.ru>
2023-09-18 01:20:25 +0300

    fix: test build

--- index.ts
+++ index.ts
@@ -1,22 +1,23 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-import {spawn, spawnSync} from 'node:child_process';
-
+import {spawnSync} from 'node:child_process';
+import process from 'node:process';
 
 try {
   const {payload: {repository}, sha } = github.context
 
   // @ts-ignore
-  const process = spawnSync('bash', ['run.bash', repository!.name, repository.clone_url, sha])
+  // const clone = spawnSync('bash', ['run.bash', repository!.name, repository.clone_url, sha])
 
-  if (process.error) {
-    throw process.error;
-  }
+  // if (clone.error) {
+  //   throw clone.error;
+  // }
 
-  console.log(process.output.toString());
+  // console.log(clone.output.toString());
 
-  const build = spawnSync('node', [`../yfm-docs/build/index.js`, '-i', 'project', '-o', 'doc'])
+  console.log(process.cwd());
+  const build = spawnSync('node', [`./yfm-docs/build/index.js`, '-i', 'project', '-o', 'doc'])
 
   if (build.error) {
     // @todo handle problem with gh pr
